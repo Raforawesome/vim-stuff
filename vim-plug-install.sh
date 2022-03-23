afd3fa706dcc214d941625c90f636474deb4323b
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-03-23 18:52:27 -0400

    Update vim-plug-install.sh

--- vim-plug-install.sh
+++ vim-plug-install.sh
@@ -1,3 +1,3 @@
-wget -O - https://www.raforaweso.me/init.vim > ~/.vimrc
+wget -O - https://www.raforaweso.me/.vimrc > ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
